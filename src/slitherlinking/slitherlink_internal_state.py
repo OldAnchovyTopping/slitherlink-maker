fe38a4bf8c105dfce60b83b9ad906374d00699a4
Pavol <palkollar9@gmail.com>
2021-09-22 16:36:58 +0200

    Testing has been set up. Also changed a couple of things in the internal state (mainly typehints).

--- src/slitherlinking/slitherlink_internal_state.py
+++ src/slitherlinking/slitherlink_internal_state.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from typing import Union
 
 
 class PathCrossingException(Exception):
@@ -27,12 +28,17 @@ class Slitherlink:
         self.height = height
         self.grid_width = 2 * self.width + 1
         self.grid_height = 2 * self.height + 1
-        even_row = list("E" + "XE" * (self.width + 1))  # With extra padding
-        odd_row = ["E" if i % 2 else 4 for i in range(self.grid_width + 2)]
+        even_row: list[Union[str, int]] =\
+            list("E" + "XE" * (self.width + 1))  # With extra padding
+        odd_row: list[Union[int, str]] =\
+            ["E" if i % 2 else 4 for i in range(self.grid_width + 2)]
         odd_row[0] = odd_row[-1] = " "  # Don't forget the padding
-        self.state_of_grid = [odd_row[:] if i % 2 else even_row[:]
-                              for i in range(self.grid_height)]
-        vertical_padding = list(" E" * (self.width + 1) + " ")
+        # : list[Union[list[Union[str, int]], list[str]]]
+        self.state_of_grid: list[list[Union[str, int]]] =\
+            [odd_row[:] if i % 2 else even_row[:]
+             for i in range(self.grid_height)]
+        vertical_padding: list[Union[str, int]] =\
+            list(" E" * (self.width + 1) + " ")
         self.state_of_grid.insert(0, vertical_padding)  # Need to pad top...
         self.state_of_grid.append(vertical_padding)  # ... And bottom too.
 
@@ -71,7 +77,7 @@ class Slitherlink:
 
         :param cell_x: The x-coordinate of the line in self.state_of_grid.
         :param cell_y: The y-coordinate of the line in self.state_of_grid.
-        :param new_number: True means erase an edge. False mean add an edge.
+        :param new_number: The new value to be put into the desired cell.
         """
         assert 1 <= cell_x <= self.height, "Row index out of bounds."
         assert 1 <= cell_y <= self.width, "Column index out of bounds."
@@ -80,7 +86,7 @@ class Slitherlink:
         true_x, true_y = 2 * cell_x, 2 * cell_y  # In self.state_of_grid.
         self.state_of_grid[true_x][true_y] = new_number
 
-    def number_of_lined_edges_around(self, true_x: int, true_y: int):
+    def number_of_lined_edges_around(self, true_x: int, true_y: int) -> int:
         """
         Counts the number of active edges around a cell or
         the number of active edges incoming into a corner.
@@ -91,10 +97,10 @@ class Slitherlink:
         """
         assert 1 <= true_x <= self.grid_height, "Row index out of bounds."
         assert 1 <= true_y <= self.grid_width, "Column index out of bounds."
-        up_edge = self.state_of_grid[true_x - 1][true_y] == "L"
-        left_edge = self.state_of_grid[true_x][true_y - 1] == "L"
-        right_edge = self.state_of_grid[true_x][true_y + 1] == "L"
-        down_edge = self.state_of_grid[true_x + 1][true_y] == "L"
+        up_edge: bool = self.state_of_grid[true_x - 1][true_y] == "L"
+        left_edge: bool = self.state_of_grid[true_x][true_y - 1] == "L"
+        right_edge: bool = self.state_of_grid[true_x][true_y + 1] == "L"
+        down_edge: bool = self.state_of_grid[true_x + 1][true_y] == "L"
         return up_edge + left_edge + right_edge + down_edge  # Bool summing!
 
     def check_a_corner(self, corner_x: int, corner_y: int) -> bool:
@@ -119,7 +125,7 @@ class Slitherlink:
         assert 0 <= cell_x <= self.height, "Row index out of bounds."
         assert 0 <= cell_y <= self.width, "Column index out of bounds."
         true_x, true_y = 2 * cell_x, 2 * cell_y  # In self.state_of_grid.
-        cell_value = self.state_of_grid[true_x][true_y]
+        cell_value: int = int(self.state_of_grid[true_x][true_y])
         adjacent_edges = self.number_of_lined_edges_around(true_x, true_y)
         return adjacent_edges <= cell_value
 
