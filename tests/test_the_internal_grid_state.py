c7ba37f49189686382a367d4343050d0e9195c1a
Pavol <palkollar9@gmail.com>
2021-09-23 23:55:26 +0200

    Couple more tests added.

--- tests/test_the_internal_grid_state.py
+++ tests/test_the_internal_grid_state.py
@@ -1,5 +1,6 @@
 from slitherlinking.slitherlink_internal_state import *
 import pytest
+from itertools import product
 
 
 @pytest.mark.parametrize("x_size, y_size", [(0, 0), (1, 13), (3, 4), (20, 50)])
@@ -38,7 +39,7 @@ def test_for_no_crossing_of_lines(lines: list[tuple]):
     new_grid = Slitherlink(6, 6)
     for x_coordinate, y_coordinate in lines:
         new_grid.change_line_segment(x_coordinate, y_coordinate, "L")
-    new_grid.check_all_corners()
+    new_grid.check_all_corners()  # No exception here.
 
 
 @pytest.mark.parametrize("cell_x, cell_y, number", [
@@ -57,12 +58,12 @@ def test_for_no_crossing_of_lines(lines: list[tuple]):
     (5, 3, 0),
     (6, 2, 0),
     (5, 5, 0),
-    (1, 4, 0),
-    (1, 5, 0),
-    (1, 6, 0),
-    (2, 4, 0),
-    (2, 5, 0),
-    (2, 6, 0),
+    (1, 4, 1),
+    (1, 5, 1),
+    (1, 6, 1),
+    (2, 4, 1),
+    (2, 5, 1),
+    (2, 6, 1),
     (4, 2, 1),
     (5, 1, 1),
     (5, 3, 1),
@@ -81,3 +82,34 @@ def test_cell_number_overloads(number_test_grid: Slitherlink,
     with pytest.raises(CellValueOverload):
         number_test_grid.change_number(cell_x, cell_y, number)
         number_test_grid.check_a_number(cell_x, cell_y)
+
+
+@pytest.mark.parametrize("number, cells", [
+    (4, [(x, y) for x, y in product((1, 2, 3, 4, 5, 6), repeat=2)]),
+    (0, [(5, 2)]),
+    (1, [(5, 2), (1, 1), (1, 2), (2, 1), (2, 2)]),
+    (2, [(5, 2), (1, 1), (1, 2), (2, 1), (2, 2),
+         (1, 4), (1, 5), (1, 6), (2, 4), (2, 5), (2, 6)]),
+    (3, [(5, 2), (1, 1), (1, 2), (2, 1), (2, 2),
+         (1, 4), (1, 5), (1, 6), (2, 4), (2, 5), (2, 6),
+         (4, 2), (5, 1), (5, 3), (6, 2)])
+])
+def test_cell_number_works(number_test_grid: Slitherlink,
+                           cells: list[int], number: int):
+    """Checks all viable number-line configurations that work."""
+    for cell_x, cell_y in cells:
+        number_test_grid.change_number(cell_x, cell_y, number)
+    number_test_grid.check_all_numbers()  # No exception here.
+
+
+@pytest.mark.parametrize("tested_number", [-20, -3, -1, 5, 6, 8, 13, 142, 211])
+def test_bad_cell_number_values(tested_number: int):
+    fresh_grid = Slitherlink(2, 2)
+    with pytest.raises(BadCellValueError):
+        fresh_grid.change_number(1, 2, tested_number)
+
+
+@pytest.mark.parametrize("tested_number", [0, 1, 2, 3, 4])
+def test_correct_cell_number_values(tested_number: int):
+    fresh_grid = Slitherlink(2, 2)
+    fresh_grid.change_number(1, 2, tested_number)  # No exception here.
